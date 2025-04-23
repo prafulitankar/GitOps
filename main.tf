2bdf327c9c73c39a806ec160092821133397b0c0
Praful Itankar <53398193+prafulitankar@users.noreply.github.com>
2025-04-23 23:51:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,90 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_ecr_repository" "my_ecr_repo" {
-  name                 = var.repository_name
-  image_tag_mutability = "MUTABLE"
+#  ECS Cluster
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = var.ecs_cluster_name
+}
 
+# 3. IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# 4. ECS Task Definition
+resource "aws_ecs_task_definition" "test" {
+  family                   = "test"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "latest"
+      image     = "114215073164.dkr.ecr.us-east-1.amazonaws.com/praful-1:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8013
+        }
+      ]
+    }
+  ])
+}
+
+# 5. ECS Service
+resource "aws_ecs_service" "app_service" {
+  name            = "app-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  launch_type     = "FARGATE"
+  desired_count   = 1
+  task_definition = aws_ecs_task_definition.test.arn
+
+  network_configuration {
+    subnets         = [aws_subnet.ecs_subnet.id]
+    assign_public_ip = true
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.ecs_execution_policy]
+}
+
+# 6. Security Group
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-sg"
+  description = "Allow HTTP"
+  vpc_id      = aws_vpc.ecs_vpc.id
+
+  ingress {
+    from_port   = 8013
+    to_port     = 8013
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
