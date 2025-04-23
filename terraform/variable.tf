ca12513fb4af05dd9d418c422bf29f0f083748e6
Praful Itankar <53398193+prafulitankar@users.noreply.github.com>
2025-04-23 21:08:27 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -8,3 +8,21 @@ variable "repository_name" {
   description = "ECR repository name"
   type        = string
 }
+
+variable "ami" {
+  description = "EC2 AMI ID"
+  type = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type = string
+}
+
+variable "subnet_id" {
+  description = "EC2 Subnet Id"
+  type = string
+}
+
+
+
