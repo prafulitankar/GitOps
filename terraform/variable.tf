1783ecf872e13f0310d5393890345636d7914cbf
Praful Itankar <53398193+prafulitankar@users.noreply.github.com>
2025-04-23 21:01:52 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "repository_name" {
+  description = "ECR repository name"
+  type        = string
+}
