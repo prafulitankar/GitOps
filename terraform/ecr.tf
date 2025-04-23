300f77e134534ede9e2467405eea5f537ec4a96e
Praful Itankar <53398193+prafulitankar@users.noreply.github.com>
2025-04-23 22:36:53 +0530

    Delete terraform/ecr.tf

--- terraform/ecr.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_ecr_repository" "my_ecr_repo" {
-  name                 = var.repository_name
-  image_tag_mutability = "MUTABLE"
-
-}
