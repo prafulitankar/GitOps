6cefd787920b193e424217d518437bbd8e1c9dff
Praful Itankar <53398193+prafulitankar@users.noreply.github.com>
2025-04-24 13:55:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,6 +95,7 @@ resource "aws_lb_target_group" "app_tg" {
   name     = "app-tg"
   port     = 80
   protocol = "HTTP"
+  vpc_id   = var.vpc_id
   target_type = "ip"  # Use "instance" for EC2 launch type
 
   health_check {
